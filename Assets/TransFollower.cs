a8f38eb2af7dc000e4632269a446d398ec397b13
Eric Li <li.zhe.hua@gmail.com>
2017-01-21 09:50:10 -0500

    Add TransFollower, for a camera that tracks another Transform

---
+++ Assets/TransFollower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransFollower : MonoBehaviour {
+    public Transform target;
+    public bool trackX;
+    public bool trackY;
+    public bool trackZ;
+
+    void Start () {
+        if (!target)
+            Debug.LogError("TransFollower has no target");
+    }
+
+
+    void Update () {
+        if (!target)
+            return;
+
+        if (!trackX && !trackY && !trackZ)
+            return;
+
+        Transform myTrans = gameObject.GetComponent<Transform>();
+        Vector3 targetPos = target.position;
+
+        Vector3 pos = myTrans.position;
+        if (trackX)
+            pos.x = targetPos.x;
+        if (trackY)
+            pos.y = targetPos.y;
+        if (trackZ)
+            pos.z = targetPos.z;
+
+        myTrans.position = pos;
+    }
+}
