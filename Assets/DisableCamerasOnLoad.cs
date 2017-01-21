c56ff3db4e735eea9271ceaaa96c3b62ecac0fda
Eric Li <li.zhe.hua@gmail.com>
2017-01-21 10:11:55 -0500

    Add DisableCamerasOnLoad
    This may be used for async scene loading, if we need to preload
    something ahead of time, without having it pop up on screen prematurely.

---
+++ Assets/DisableCamerasOnLoad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCamerasOnLoad : MonoBehaviour {
+    List<Camera> sceneCameras = new List<Camera>();
+
+    void Start() {
+        sceneCameras.Clear();
+        foreach (var root in gameObject.scene.GetRootGameObjects())
+        {
+            foreach (var camera in root.GetComponentsInChildren<Camera>())
+            {
+                camera.enabled = false;
+                sceneCameras.Add(camera);
+            }
+        }
+    }
+}
