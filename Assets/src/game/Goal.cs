de551f9d2c2d771a7f80ad7af734cdfb91159532
Eric Li <li.zhe.hua@gmail.com>
2017-01-21 22:34:49 -0500

    Add arena, ball, and goal

---
+++ Assets/src/game/Goal.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.GetComponent<Ball>()) {
+            print("Goal!");
+        }
+    }
+}
