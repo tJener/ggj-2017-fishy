cf8af443ea61cfd01d3f6755c03261bf2f915ef4
Eric Li <li.zhe.hua@gmail.com>
2017-01-21 23:12:14 -0500

    Failed attempt at getting mesh colliders attached to bones

--- Assets/src/game/SwordyBody.cs
+++ Assets/src/game/SwordyBody.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class MeshBonePair {
+    public Mesh sharedMesh;
+    public GameObject bone;
+}
+
 public class SwordyBody : MonoBehaviour {
+    public List<MeshBonePair> meshBonePairs;
     public List<SkinnedMeshRenderer> collidableGeo;
     public float skinWidth = 0.01f;
 
@@ -14,5 +21,15 @@ public class SwordyBody : MonoBehaviour {
             collider.skinWidth = skinWidth;
             collider.sharedMesh = smr.sharedMesh;
         }
+
+        // foreach (var pair in meshBonePairs) {
+        //     GameObject colliderObj = new GameObject("collider");
+        //     colliderObj.transform.SetParent(pair.bone.transform, false);
+        //     MeshCollider collider = colliderObj.AddComponent<MeshCollider>();
+        //     collider.convex = true;
+        //     collider.inflateMesh = true;
+        //     collider.skinWidth = skinWidth;
+        //     collider.sharedMesh = pair.sharedMesh;
+        // }
     }
 }
