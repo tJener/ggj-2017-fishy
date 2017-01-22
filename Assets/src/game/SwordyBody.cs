0397f9b012617f7958c449761db0f8ccd2795449
Eric Li <li.zhe.hua@gmail.com>
2017-01-21 21:27:54 -0500

    Fix colliders for new swordy geo
    Set up swordy prefab so that new FBXs can just be slammed in under the
    model GameObject.

--- Assets/src/game/SwordyBody.cs
+++ Assets/src/game/SwordyBody.cs
@@ -3,4 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwordyBody : MonoBehaviour {
+    public List<SkinnedMeshRenderer> collidableGeo;
+    public float skinWidth = 0.01f;
+
+    void Start() {
+        foreach (var smr in collidableGeo) {
+            MeshCollider collider = smr.gameObject.AddComponent<MeshCollider>();
+            collider.convex = true;
+            collider.inflateMesh = true;
+            collider.skinWidth = skinWidth;
+            collider.sharedMesh = smr.sharedMesh;
+        }
+    }
 }
