bfffc7ec496844b75aedbef0841012ef2007284a
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-01-22 11:31:20 -0500

    Round start and end controller

---
+++ Assets/src/game/score/RoundStart.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundStart : MonoBehaviour {
+
+  public GameObject resetObject;
+  public Text countdownText;
+  public ScoreTracker scoreTracker;
+
+  public GameObject roundObject;
+
+	// Use this for initialization
+	void Start () {
+    scoreTracker.callback += GoalCallback;
+
+    StartRound();
+	}
+
+  void StartRound() {
+		var obj = Object.Instantiate(resetObject);
+    roundObject = obj;
+    obj.SetActive(true);
+
+    var controllers = obj.GetComponentsInChildren<WavePhysicsController>();
+    DisableControllers(controllers);
+    StartCoroutine(CountdownStart(controllers));
+  }
+
+  void GoalCallback() {
+    StartCoroutine(AnimateRoundEnd());
+  }
+
+  IEnumerator AnimateRoundEnd() {
+    var controllers = roundObject.GetComponentsInChildren<WavePhysicsController>();
+    DisableControllers(controllers);
+    foreach (var c in controllers) {
+      var r = c.GetComponent<Rigidbody>();
+      r.useGravity = true;
+      r.constraints = RigidbodyConstraints.None;
+    }
+
+    scoreTracker.ToggleSize(ScoreTracker.Size.Large);
+
+    yield return new WaitForSeconds(3);
+
+    Object.Destroy(roundObject);
+
+    scoreTracker.ToggleSize(ScoreTracker.Size.Small);
+
+    StartRound();
+  }
+
+  void SetCountdown(string text) {
+    if (countdownText != null) {
+      countdownText.text = text;
+    }
+  }
+
+  IEnumerator CountdownStart(WavePhysicsController[] controllers) {
+    for (int i = 0; i < 3; i++) {
+      SetCountdown((3 - i).ToString());
+      yield return new WaitForSeconds(1);
+    }
+
+    SetCountdown("GO!");
+    EnableControllers(controllers);
+    yield return new WaitForSeconds(1);
+
+    SetCountdown("");
+  }
+
+  void DisableControllers(WavePhysicsController[] controllers) {
+    foreach (var c in controllers) {
+      c.enabled = false;
+    }
+  }
+
+  void EnableControllers(WavePhysicsController[] controllers) {
+    foreach (var c in controllers) {
+      c.enabled = true;
+    }
+  }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/src/game/score/ScoreTracker.cs
+++ Assets/src/game/score/ScoreTracker.cs
@@ -13,12 +13,21 @@ public class ScoreTracker : MonoBehaviour {
   public Text rightText;
   public Goal rightGoal;
 
+  public Text leftLargeText;
+  public Text rightLargeText;
+
+  public delegate void GoalCallback();
+  public GoalCallback callback;
+
 	// Use this for initialization
 	void Start () {
     leftGoal.callback += ScoreLeft;
     rightGoal.callback += ScoreRight;
     leftText.text = "0";
     rightText.text = "0";
+    leftLargeText.text = "0";
+    rightLargeText.text = "0";
+    ToggleSize(Size.Small);
 	}
 	
 	// Update is called once per frame
@@ -29,10 +38,31 @@ public class ScoreTracker : MonoBehaviour {
   void ScoreLeft() {
     leftScore += 1;
     leftText.text = leftScore.ToString();
+    leftLargeText.text = leftScore.ToString();
+    PublishGoal();
   }
 
   void ScoreRight() {
     rightScore += 1;
     rightText.text = rightScore.ToString();
+    rightLargeText.text = rightScore.ToString();
+  }
+
+  void PublishGoal() {
+    if (callback != null) {
+      callback();
+    }
+  }
+
+  public enum Size {
+    Small,
+    Large,
+  }
+
+  public void ToggleSize(Size size) {
+    leftLargeText.enabled = size == Size.Large;
+    rightLargeText.enabled = size == Size.Large;
+    leftText.enabled = size == Size.Small;
+    rightText.enabled = size == Size.Small;
   }
 }
