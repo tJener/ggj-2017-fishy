d117b99d872e8774fd186a13c8ee2e3e5cc0d56f
Michael "Z" Goddard <mzgoddard@gmail.com>
2017-01-22 10:16:53 -0500

    Add score counter

--- Assets/src/game/Goal.cs
+++ Assets/src/game/Goal.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Goal : MonoBehaviour {
+
+    public delegate void GoalCallback();
+    public event GoalCallback callback;
+
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.GetComponent<Ball>()) {
             print("Goal!");
+            if (callback != null) {
+                callback();
+            }
         }
     }
 }
---
+++ Assets/src/game/score/ScoreTracker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour {
+
+  public int leftScore;
+  public Text leftText;
+  public Goal leftGoal;
+
+  public int rightScore;
+  public Text rightText;
+  public Goal rightGoal;
+
+	// Use this for initialization
+	void Start () {
+    leftGoal.callback += ScoreLeft;
+    rightGoal.callback += ScoreRight;
+    leftText.text = "0";
+    rightText.text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  void ScoreLeft() {
+    leftScore += 1;
+    leftText.text = leftScore.ToString();
+  }
+
+  void ScoreRight() {
+    rightScore += 1;
+    rightText.text = rightScore.ToString();
+  }
+}
